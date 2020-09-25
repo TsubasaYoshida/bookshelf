62f78c8c30c41536d0b770cab70c8a8f6304f424
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-09-25 16:17:47 +0900

    不要なページを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :books
   resources :done_items
   devise_for :users
-  get 'home/index'
-  root to: 'home#index'
+  root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
